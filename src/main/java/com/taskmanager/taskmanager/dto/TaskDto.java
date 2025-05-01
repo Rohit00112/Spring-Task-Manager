e7c5e87c4d087fcd633621899dd5709c33c15fa5
Sujan Subedi <subedirohit49@gmail.com>
2025-05-01 10:49:51 +0545

    feat: create dto for User and Task

---
+++ src/main/java/com/taskmanager/taskmanager/dto/TaskDto.java
@@ -0,0 +1,9 @@
+package com.taskmanager.taskmanager.dto;
+
+public class TaskDto {
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private String dueDate;
+}
